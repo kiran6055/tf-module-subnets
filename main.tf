2c9e6cd588af109997cf889460860edc360a6888
kiran1616 <kirankumar7163@gmail.com>
2023-04-05 00:07:09 +0530

    updated script

--- main.tf
+++ main.tf
@@ -1,42 +1,53 @@
-# creating a subnets
-resource "aws_subnet" "main" {
-  count               = length(var.cidr_block)
-  cidr_block          = var.cidr_block[count.index]
-  availability_zone   = var.availability_zone[count.index]
-  vpc_id              = var.vpc_id
-  tags       = merge(
-    local.common_tags,
-    { Name = "${var.env}-${var.name}-subnet-${count.index+1}" }
-  )
+module "subnets" {
+  source                    = "./subnets"
+  availability_zone         = var.availability_zone
+  cidr_block                = var.cidr_block
+  default_vpc_id            = var.default_vpc_id
+  env                       = var.env
+  name                      = var.name
+  vpc_id                    = var.vpc_id
+  vpc_peering_connection_id = var.vpc_peering_connection_id
 }
 
-# creating aws route table
-resource "aws_route_table" "route_table" {
-  vpc_id = var.vpc_id
+#resource "aws_route" "internet_gw_route" {
+#  count                  = var.internet_gw ? 1 : 0
+#  route_table_id         = aws_route_table.route_table.id
+#  destination_cidr_block = "0.0.0.0/0"
+#  gateway_id             = var.internet_gw_id
 
-  route {
-    cidr_block                = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = var.vpc_peering_connection_id
-  }
+#}
 
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-${var.name}-route_table" }
-  )
-}
+# creating internet gateway
+#resource "aws_internet_gateway" "igw" {
+#  count                  = var.internet_gw ? 1 : 0
+#  vpc_id = var.vpc_id
 
-# creating route table association
-resource "aws_route_table_association" "association" {
-  count          = length(aws_subnet.main)
-  subnet_id      = aws_subnet.main.*.id[count.index]
-  route_table_id = aws_route_table.route_table.id
-}
+#  tags       = merge(
+#    local.common_tags,
+#    { Name = "${var.env}-igw" }
+#  )
+#}
+
+# associating nate gateway to public sunet, in office we need to creat 2 or mutliple nat gateway based on requirment but for lab we created only one
+#resource "aws_nat_gateway" "ngw" {
+#  count                  = var.nat_gw ? 1 : 0
+#  allocation_id = aws_eip.ngw-eip.id
+#  subnet_id     = var.public_subnets_ids[0]
+
+#  tags       = merge(
+#    local.common_tags,
+#    { Name = "${var.env}--ngw" }
+#  )
+
+#}
 
 
-resource "aws_route" "internet_gw_route" {
-  count                  = var.internet_gw ? 1 : 0
-  route_table_id         = aws_route_table.route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = var.internet_gw_id
 
-}
+
+
+# creating elastice ip
+#resource "aws_eip" "ngw-eip" {
+#  vpc      = true
+#}
+
+
