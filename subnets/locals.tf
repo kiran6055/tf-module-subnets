2bbd140879c08f18187ad56956b33be6afabb309
kiran1616 <kirankumar7163@gmail.com>
2023-04-10 14:46:15 +0530

    updated script

--- subnets/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  common_tags = {
-    env = var.env
-    project = "roboshop"
-    business_unit = "ecommerce"
-    owner = "IBM-ecommerce"
-    name = "kiran"
-  }
-}
--- subnets/vars.tf
+++
@@ -1,7 +0,0 @@
-variable "cidr_block" {}
-variable "availability_zone" {}
-variable "vpc_id" {}
-variable "env" {}
-variable "default_vpc_id" {}
-variable "name" {}
-variable "vpc_peering_connection_id" {}
